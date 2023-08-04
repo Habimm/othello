bf7595666531c7da3245b202061f3c200c84568a
Dimitri Rusin <dimitri@habimm.com>
2023-08-04 07:41:15 +0200

    make a sequence of models and save the training data; also evaluate before training

--- predict.py
+++ predict.py
@@ -1,9 +1,11 @@
 import ast
 import numpy
+import os
 import pandas
 import random
 import tensorflow
 import tensorflow.keras
+import tensorflow.keras.callbacks
 import tensorflow.keras.layers
 
 # Load the decomposed games data.
@@ -17,8 +19,10 @@ tensorflow.random.set_seed(RANDOM_NUMBERS_SEQUENCE_NAME)
 
 # model_load_path = 'generated/othello_model.keras-1000.keras-1000.keras'
 # model_load_path = 'generated/othello_model.keras-1000.keras'
-model_load_path = 'generated/othello_model.keras'
-# model_load_path = None
+# model_load_path = 'generated/othello_model.keras'
+model_load_path = None
+
+# Check if the model load path exists
 if not model_load_path:
   # Define the oracle neural network's architecture
   inputs = tensorflow.keras.Input(shape=(2, 8, 8)) # input shape is (8, 8, 2) - two 8x8 planes
@@ -31,25 +35,59 @@ if not model_load_path:
 else:
   model = tensorflow.keras.models.load_model(model_load_path)
 
+os.makedirs("generated/models/eOthello", exist_ok=True)
+
+# Save initial model before training begins
+filepath = "generated/models/eOthello/eOthello-{epoch}.keras"
+initial_save_path = filepath.replace('{epoch}', '0')
+model.save(initial_save_path)
+
+# Prepare callback to save model every 100 epochs
+checkpoint_callback = tensorflow.keras.callbacks.ModelCheckpoint(
+  filepath, monitor='loss', verbose=1,
+  save_best_only=False, save_weights_only=False,
+  mode='auto', save_freq='epoch', options=None
+)
+
 # Use ast.literal_eval to convert strings to lists, then convert lists to numpy arrays
-boards = numpy.array(othello_actions_dataframe['Board'].apply(ast.literal_eval).apply(numpy.array).tolist())
+boards = othello_actions_dataframe['Board']
+boards = boards.apply(ast.literal_eval)
+boards = boards.apply(numpy.array)
+boards = boards.tolist()
+boards = numpy.array(boards)
 batch_size = 64
-epochs = 1000
-history = model.fit(boards, othello_actions_dataframe['Real_Player_Outcome'], batch_size=batch_size, epochs=epochs)
-if model_load_path:
-  model_save_path = f'{model_load_path}-{epochs}.keras'
-else:
-  model_save_path = f'generated/othello_model.keras'
-model.save(model_save_path)
+epochs = 1_000
 
-othello_actions_dataframe['Predicted_Player_Outcome'] = model.predict(boards)
+initial_loss, initial_accuracy = model.evaluate(
+  boards,
+  othello_actions_dataframe['Real_Player_Outcome'],
+  verbose=0
+)
 
-# Save some training statistics to CSV file.
-hist_df = pandas.DataFrame(history.history)
-hist_df.reset_index(inplace=True)
-hist_df.rename(columns={'index': 'Epoch'}, inplace=True)
-hist_df['Epoch'] += 1
-hist_df = hist_df.iloc[::-1]
-hist_df.to_csv('generated/training_history.csv', index=False)
+# Train the model with the new callback
+history = model.fit(
+  boards,
+  othello_actions_dataframe['Real_Player_Outcome'],
+  batch_size=batch_size,
+  epochs=epochs,
+  callbacks=[checkpoint_callback]
+)
 
-othello_actions_dataframe.to_csv('generated/othello_predictions.csv', sep=';')
+# Save the loss dataframe to a CSV file
+history_df = pandas.DataFrame(history.history)
+history_df.reset_index(inplace=True)
+history_df.rename(columns={'index': 'epoch'}, inplace=True)
+history_df['epoch'] += 1
+
+# Create a DataFrame for initial metrics and append it to the history DataFrame
+initial_metrics_df = pandas.DataFrame({
+    'epoch': [0],
+    'loss': [initial_loss],
+    'mean_absolute_error': [initial_accuracy]
+})
+history_df = pandas.concat([initial_metrics_df, history_df])
+
+history_df = history_df.iloc[::-1]
+
+# Save the dataframe to a CSV file
+history_df.to_csv('generated/training_history.csv', index=False)
