b518d02c080ecfa7a7ab42fd1143dbda80467aec
Dimitri Rusin <dimitri@habimm.com>
2023-08-14 21:24:46 +0200

    implement playing against the random player

--- play_against_random.py
+++ play_against_random.py
@@ -2,12 +2,22 @@ from info import info
 import csv
 import multiprocessing
 import os
-import os
 import pandas
 import random
 import rules.othello
+import sys
 
-number_of_games = 20
+def get_env_variable(name):
+  value = os.environ.get(name)
+  if value is None:
+    print(f"Error: Environment variable {name} not set.")
+    sys.exit(1)
+  return value
+
+generated_path = get_env_variable('OTHELLO_GENERATED_PATH')
+number_of_games = int(get_env_variable('OTHELLO_NUMBER_OF_GAMES'))
+
+
 
 def index_to_letter(index):
   return chr(index + ord('a')).upper()
@@ -69,6 +79,57 @@ def play_games(model_load_path, num_plays_for_evaluation):
       print(f'Written row to {play_save_path}.')
   return play_save_path
 
+def play_games_against_random(num_plays_for_evaluation):
+  play_save_path = f'{generated_path}/plays/random/random.csv'
+  for _ in range(num_plays_for_evaluation):
+    game = rules.othello.Othello(should_draw_tiles=False)
+    game.draw_board()
+    game.initialize_board()
+    game.current_player = 0
+    other_player_has_a_move = True
+    moves = []
+    black_outcome = None
+    random_integer = random.randint(0, 100_000)
+    random_integer_str = str(random_integer).zfill(5)
+    game_id = f'{random_integer_str}'
+    while True:
+      # Check if the player pointed at by `game.current_player` has a legal move.
+      # So, if there is a legal move from the current game state (board + player).
+      if game.has_legal_move():
+        other_player_has_a_move = True
+        move = None
+        if game.current_player == 0: # == BLACK PLAYER
+          # BLACK is random
+          move = game.make_random_move()
+        if game.current_player == 1: # == WHITE PLAYER
+          # WHITE is trained
+          move = game.make_random_move()
+        assert move is not None
+        moves.append(move)
+      else:
+        if other_player_has_a_move is False:
+          if game.num_tiles[0] > game.num_tiles[1]:
+            black_outcome = 1
+          elif game.num_tiles[0] < game.num_tiles[1]:
+            black_outcome = -1
+          else:
+            black_outcome = 0
+          break
+        other_player_has_a_move = False
+      game.current_player = 1 - game.current_player
+
+    assert black_outcome is not None
+    translated_moves = [convert_index_to_chess_notation(move) for move in moves]
+    moves_concatenation = ''.join(translated_moves).lower()
+    row = f'{game_id},RANDOM_PLAYER,RANDOM_PLAYER,{black_outcome},{moves_concatenation}'
+    if not os.path.exists(play_save_path):
+      with open(play_save_path, 'w') as random_evaluation_file:
+        print('evaluation_game_id,model_load_path,baseline,black_outcome,game_moves', file=random_evaluation_file)
+    with open(play_save_path, 'a') as random_evaluation_file:
+      print(row, file=random_evaluation_file)
+      print(f'Written row to {play_save_path}.')
+  return play_save_path
+
 # Update CSV in a thread-safe manner
 def update_csv(filename, data):
   with multiprocessing.Lock():
@@ -94,13 +155,13 @@ def get_files_from_directory(directory):
 
 def main():
   random.seed(1)
-  models_directory = 'generated_200_plays/models/eOthello/'
+  models_directory = f'{generated_path}/models/eOthello-1/'
   model_load_paths = get_files_from_directory(models_directory)
   model_load_paths.sort()
   play_directory = models_directory.replace('models', 'plays', 1)
   os.makedirs(play_directory, exist_ok=True)
 
-  directory_path = 'generated_200_plays/models/eOthello/'
+  directory_path = f'{generated_path}/models/eOthello-1/'
   model_load_paths = get_files_from_directory(directory_path)
   num_processes = min(multiprocessing.cpu_count(), len(model_load_paths))
 
@@ -112,11 +173,14 @@ def main():
   for _ in range(num_processes):
     job_queue.put(None)
 
-  csv_filename = 'generated_200_plays/winning_rates.csv'
+  csv_filename = f'{generated_path}/winning_rates.csv'
   # Initialize the CSV with headers
   with open(csv_filename, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['play_path', 'number_of_wins', 'number_of_games'])
+
+  # draw yellow horizontal baseline in stories.py into the diagram
+  # play_games_against_random(number_of_games)
 
   processes = []
   for _ in range(num_processes):
