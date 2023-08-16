b2dd85ea9539b471f7fc1be496edc4c5782db7c5
Dimitri Rusin <dimitri@habimm.com>
2023-08-16 20:24:01 +0200

    monte carlo

--- mcts.py
+++ mcts.py
@@ -1,4 +1,5 @@
 import rules.othello
+import math
 import copy
 from info import info
 
@@ -7,15 +8,15 @@ from info import info
 
 
 
-
-
+NUM_SIMULATIONS = 20
+c_puct = 0.35
 
 class Node:
 
   def __init__(self, state, parent=None):
     self.accumulated_outcome = 0
     self.visits = 0
-    self.probability = 0
+    self.probability = 1
     self.state = state
     self.children = []
     self.parent = parent
@@ -35,20 +36,29 @@ class Node:
       next_state.move = move
       next_state.make_move()
       next_state.current_player = 1 - next_state.current_player
-      child = Node(next_state)
+      child = Node(next_state, self)
       self.children.append(child)
 
   def backpropagate(self, outcome):
-    node = self
-    node.visits += 1
-    node.accumulated_outcome += outcome
-    while node.parent:
-      node.backpropagate(outcome)
-      node = node.parent
+    self.visits += 1
+    self.accumulated_outcome += outcome
+    if self.parent:
+      self.parent.backpropagate(outcome)
 
   def select(self):
-    child = self.children[0]
-    return child
+    visits_sum = sum(child.visits for child in self.children)
+    visits_sum = math.sqrt(visits_sum)
+    best_score = float('-inf')
+    best_child = None
+    for child in self.children:
+      score = c_puct * child.probability * visits_sum / (1 + child.visits)
+      if score > best_score:
+        best_score = score
+        best_child = child
+
+    assert best_child is not None
+
+    return best_child
 
   def evaluate(self):
     return 0.7
@@ -58,6 +68,28 @@ class Node:
     while node.children:
       node = node.select()
     node.expand()
+
+  def search(self):
+    for _ in range(NUM_SIMULATIONS):
+      self.simulate()
+
+  def to_dot(self):
+    lines = ['digraph Tree {']
+
+    def traverse(node, parent_id=None):
+      node_id = f"node_{id(node)}"
+      node_label = f"accumulated_outcome={node.accumulated_outcome}\nvisits={node.visits}\nprobability={node.probability}\nstate={node.state}"
+      lines.append(f'{node_id} [label="{node_label}"];')
+
+      if parent_id:
+        lines.append(f'{parent_id} -> {node_id};')
+
+      for child in node.children:
+        traverse(child, node_id)
+
+    traverse(self)
+    lines.append('}')
+    return '\n'.join(lines)
 
   def _repr_helper(self, indent=0):
     # Represent current node
@@ -85,9 +117,9 @@ state = rules.othello.Othello()
 # If the following function isn't called,
 # self.state.get_legal_moves() will be an empty list.
 state.initialize_board()
-root = Node(state)
-root.simulate()
-root.simulate()
-root.simulate()
-root.simulate()
-info(root)
+root = Node()
+root.search()
+
+# Write the dot string to a file
+with open('tree.dot', 'w') as f:
+    f.write(root.to_dot())
