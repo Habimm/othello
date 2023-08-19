60a766cd74a4e0cfc4dc47b0dbd0b957cb1fcb65
Dimitri Rusin <dimitri@habimm.com>
2023-08-19 09:52:00 +0200

    some configuration to run the mcts programs

--- config.py
+++ config.py
@@ -1,23 +1,25 @@
-from datetime import datetime
-import json
+import datetime
 
-# 1. Generate the timestamp
-now = datetime.now()
-timestamp = now.strftime("generated_%Y%m%d%H%M%S")
-print(f'set -gx OTHELLO_GENERATED_PATH {timestamp}')
+# 1. Generate the timestamp_of_now
+now = datetime.datetime.now()
+timestamp_of_now = now.strftime("output/%Y%m%d%H%M%S")
 
 # 2. Configuration values from your JSON string
 config = {
   "batch_size": 64,
-  "epochs": 1000,
+  "epochs": 100,
   "num_epochs_per_checkpoint": 100,
   "num_games_for_supervised_training": 10,
-  "number_of_games": 20,
-  "RANDOM_NUMBERS_SEQUENCE_NAME": 1
+  "number_of_games": 2,
+  # "output_path": 'generated_20230814012642_mcts',
+  "output_path": '{timestamp_of_now}',
+  "random_seed": 42,
 }
 
 # 3. Print fish shell commands to set the environment variables
 for key, value in config.items():
   # Convert the key to uppercase for the environment variable name
   env_var_name = f"OTHELLO_{key.upper()}"
+  if key == 'output_path':
+    value = value.replace('{timestamp_of_now}', timestamp_of_now)
   print(f'set -gx {env_var_name} {value}')
