4487d64e48c8b1c406ad87975111ecbf1e050259
Dimitri Rusin <dimitri@habimm.com>
2023-08-25 04:22:37 +0200

    use 8 processes to play evaluation games

--- config.py
+++ config.py
@@ -7,15 +7,15 @@ timestamp_of_now = now.strftime('output/%Y%m%d%H%M%S')
 # 2. Configuration values from your JSON string
 config = {
   'batch_size': 64,
-
   # Increase this to incentivize using explorative actions
   'c_puct': 4,
-
   'epochs': 100,
   'num_epochs_per_checkpoint': 100,
   'num_games_for_supervised_training': 10,
+  'num_processes': 8,
+  # 'num_processes': multiprocessing.cpu_count(),
   'num_simulations': 12,
-  'number_of_games': 1,
+  'number_of_games': 10,
   'oracle_url': 'http://localhost:8000/predict',
   'output_path': 'output/generated_20230814012642',
   # 'output_path': 'output/generated_20230814012642_mcts',
