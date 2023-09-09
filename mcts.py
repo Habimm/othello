0b6f043abdbab851f1246f6e5981a9ec3dd7c384
Dimitri Rusin <dimitri@habimm.com>
2023-09-09 10:47:09 +0200

    vectors in the tree nodes

--- mcts.py
+++ mcts.py
@@ -1,5 +1,4 @@
 import copy
-import variables_info
 import math
 import multiprocessing
 import numpy
@@ -7,6 +6,9 @@ import os
 import requests
 import rules.othello
 import sys
+import info
+
+info.set_log_file('mcts.log')
 
 def get_env_variable(name):
   value = os.environ.get(name)
@@ -67,6 +69,12 @@ class Node:
     else:
       self.moving_along = False
 
+    self.accumulated_relative_values = None
+    self.visit_counts = None
+    self.probabilities = None
+    self.average_relative_values = None
+    self.sum_visit_counts = None
+
   def expand(self):
     moves = self.state.get_legal_moves()
     for move in moves:
@@ -90,32 +98,57 @@ class Node:
       child = Node(self.model_load_path, next_state, self, move)
       self.children.append(child)
 
+    number_of_children = len(self.children)
+    self.accumulated_relative_values = numpy.zeros(number_of_children, dtype=numpy.float64)
+    self.average_relative_values = numpy.zeros(number_of_children, dtype=numpy.float64)
+    self.visit_counts = numpy.zeros(number_of_children, dtype=numpy.int64)
+    self.probabilities = numpy.ones(number_of_children, dtype=numpy.float64)
+    self.sum_visit_counts = 0
+
   def backpropagate(self, black_outcome):
-    self.visits += 1
-    assert self.is_final or self.children, f"No children, although we have visited this node! {self}"
-    assert self.state.current_player in [0, 1], "When accumulating relative outcome, the player is either 0 (black) or 1 (white)."
-    if self.state.current_player == 0: self.accumulated_relative_outcome += black_outcome
-    if self.state.current_player == 1: self.accumulated_relative_outcome -= black_outcome
-    if self.parent and self.parent.parent:
-      self.parent.backpropagate(black_outcome)
+    if not self.parent: return
+
+    self_index = self.parent.children.index(self)
+    self.parent.visit_counts[self_index] += 1
+    self.parent.sum_visit_counts += 1
+
+    # Calculate the new average incrementally
+    old_average = self.parent.average_relative_values[self_index]
+    new_value = self.parent.accumulated_relative_values[self_index]
+    count = self.parent.visit_counts[self_index]
+
+    if self.parent.state.current_player == 0:
+      self.parent.accumulated_relative_values[self_index] += black_outcome
+      new_value += black_outcome
+    elif self.parent.state.current_player == 1:
+      self.parent.accumulated_relative_values[self_index] -= black_outcome
+      new_value -= black_outcome
+
+    # Update the average incrementally
+    self.parent.average_relative_values[self_index] = old_average + (new_value - old_average) / count
+
+    self.parent.backpropagate(black_outcome)
 
   def best_move(self):
-    visits = [child.visits for child in self.children]
-    index_of_max_visits = numpy.argmax(visits)
+    index_of_max_visits = numpy.argmax(self.visit_counts)
     child_with_most_visits = self.children[index_of_max_visits]
     child_with_most_visits_move = child_with_most_visits.move
     return child_with_most_visits_move
 
   def sample_move(self):
-    visits = [child.visits for child in self.children]
-    chosen_child = numpy.random.choice(self.children, p=visits/numpy.sum(visits))
+    if self.sum_visit_counts == 0:
+      return self.children[0].move
+    visit_counts_distribution = self.visit_counts / self.sum_visit_counts
+    chosen_child = numpy.random.choice(self.children, p=visit_counts_distribution)
     return chosen_child.move
 
   def next_node(self, move, current_player):
     move_child = None
-    for child in self.children:
+    move_child_index = None
+    for child_index, child in enumerate(self.children):
       if child.move == move:
         move_child = child
+        move_child_index = child_index
         break
 
     assert move_child is not None, f"No child to make the move {move}. Expand first!"
@@ -123,7 +156,7 @@ class Node:
     # This is needed, in case we do NOT use MCTS scores (using a number of simulations > 0)
     # to select a move.
     # We might, for example, select a move on behalf of the opponent.
-    if move_child.visits == 0:
+    if self.visit_counts[move_child_index] == 0:
       assert not move_child.children, "An unvisited leaf cannot have children!"
       move_child.expand()
     move_child.parent = None
@@ -131,16 +164,9 @@ class Node:
     return move_child
 
   def select(self):
-    squared_sum_of_visits = sum(child.visits for child in self.children)
-    squared_sum_of_visits = math.sqrt(squared_sum_of_visits)
-    best_score = float('-inf')
-    best_child = None
-    for child in self.children:
-      score = child.accumulated_relative_outcome / (1 + child.visits) + C_PUCT * child.probability * squared_sum_of_visits / (1 + child.visits)
-      if score > best_score:
-        best_score = score
-        best_child = child
-
+    uct_scores = self.average_relative_values + C_PUCT * self.probabilities * numpy.sqrt(self.sum_visit_counts) / (1 + self.visit_counts)
+    best_child_index = numpy.argmax(uct_scores)
+    best_child = self.children[best_child_index]
     assert best_child is not None
     return best_child
 
@@ -155,10 +181,9 @@ class Node:
       }
       evaluation = requests.post(OTHELLO_ORACLE_URL, json=oracle_command).json()
       if evaluation['exception']:
-        variables_info.d(evaluation)
-        variables_info.d(evaluation['exception'])
+        info.d(evaluation)
+        info.d(evaluation['exception'])
         sys.exit(1)
-      variables_info.d(evaluation)
       current_outcome = evaluation['prediction'][0][0]
       black_outcome = None
       if self.state.current_player == 0: black_outcome = current_outcome
@@ -172,19 +197,28 @@ class Node:
     while node.children:
       node = node.select()
     black_outcome = node.evaluate()
-    node.backpropagate(black_outcome)
     node.expand()
+    node.backpropagate(black_outcome)
 
   def search(self):
     for _ in range(NUM_SIMULATIONS):
       self.simulate()
 
   def to_dot(self):
-    lines = ['digraph Othello_MonteCarloTreeSearch {']
+    lines = ['digraph MCTS {']
 
     def traverse(node, parent_id=None):
       node_id = f"node_{id(node)}"
-      node_label = f"average_outcome={node.accumulated_relative_outcome / (node.visits + 1)}\naccumulated_relative_outcome={node.accumulated_relative_outcome}\nvisits={node.visits}\nprobability={node.probability}\nstate={node.state}"
+      node_label = f"""
+      node_id={node_id}
+      accumulated_relative_values={node.accumulated_relative_values}
+      average_relative_values={node.average_relative_values}
+      visit_counts={node.visit_counts}
+      probabilities={node.probabilities}
+      sum_visit_counts={node.sum_visit_counts}
+      children={[id(child) for child in node.children]}
+      state={node.state}
+      """
 
       # Conditional background coloring based on the 'moving_along' attribute
       if node.moving_along:
@@ -214,9 +248,9 @@ class Node:
     # Represent current node
     lines = [' ' * indent + f'Node:']
     next_indentation = ' ' * (indent + 2)
-    lines.append(next_indentation + f'accumulated_relative_outcome: {self.accumulated_relative_outcome}')
-    lines.append(next_indentation + f'visits: {self.visits}')
-    lines.append(next_indentation + f'probability: {self.probability}')
+    lines.append(next_indentation + f'accumulated_relative_values: {self.accumulated_relative_values}')
+    lines.append(next_indentation + f'visit_counts: {self.visit_counts}')
+    lines.append(next_indentation + f'probabilities: {self.probabilities}')
     lines.append(next_indentation + f'legal_moves: {self.state.get_legal_moves()}')
     state_representation = f'{self.state}'
     state_representation = state_representation.replace('\n', '\n' + next_indentation)
